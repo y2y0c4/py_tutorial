a661d20072bb0aada4eeea3cbdac495e65d6ac31
root <y2y0c4@gmail.com>
2015-08-24 23:20:55 +0900

    update for writing diamond_txt

--- week1/drawdia/DrawDiamond.py
+++ week1/drawdia/DrawDiamond.py
@@ -1,29 +1,32 @@
 
 def DrawDiamond(height):
-	a = []
-	if(height<22):
-		index=0
-		for i in range(1,height+1,2):
-			a.append('')
-			for j in range(height-int(i/2)):
-				a[index]+=" "
+	with open("diamond.txt",'w') as f:
+		a = []
+		if(height<22):
+			index=0
+			for i in range(1,height+1,2):
+				a.append('')
+				for j in range(height-int(i/2)):
+					a[index]+=" "
 
-			for j in range(i):
-				a[index]+="*"
-			index+=1
+				for j in range(i):
+					a[index]+="*"
+				index+=1
 			
-		a.append('-')
-		for i in range(len(a)):
-			if(a[i]!='-'):
-				print(a[i]);
-			else:
-				a.reverse()
-				for i in range(len(a)):
-					if(a[i]=='-'):
-						None
-					elif(i==1):
-						None
-					else:
-						print(a[i])
-	else:
-		print("It's too long")
+			a.append('-')
+			for i in range(len(a)):
+				if(a[i]!='-'):
+					print(a[i])
+					f.write(a[i]+"\n")
+				else:
+					a.reverse()
+					for i in range(len(a)):
+						if(a[i]=='-'):
+							None
+						elif(i==1):
+							None
+						else:
+							print(a[i])
+							f.write(a[i]+"\n")
+		else:
+			print("It's too long")
