5069d16ff9b3cc531ec5021989c694c9d63fe27a
root <y2y0c4@gmail.com>
2015-08-28 11:23:33 +0900

    add day5

--- day5/test.py
+++ day5/test.py
@@ -7,12 +7,12 @@ res = urllib.request.urlopen(url)
 html = res.read()
 
 #pprint.pprint(html)
-
+pre_url = b'''"/webtoon/viewer_images.js?webtoon_episode_id=32683"'''
 pattern = b'''img src="(http://i1.cartoon.daumcdn.net/svc/image/U03/cartoon/)(.*?)"'''
 
-match=re.findall(pattern, html)
+match=re.findall(pre_url, html)
 
 pprint.pprint(match)
-wanted_str = str(match[0][0])[2:-1]+str(match[0][1])[2:-1]
-print(wanted_str)
-os.system('wget '+ wanted_str)
+#wanted_str = str(match[0][0])[2:-1]+str(match[0][1])[2:-1]
+#print(wanted_str)
+#os.system('wget '+ wanted_str)
